05f3b4987e32321695a737e9de908f92b8a5b014
Zachary Cecchetti <zcecchetti@gmail.com>
2022-03-19 18:47:55 -0700

    Wrote functions to make new instances of object and started formating/styling webpage

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 
-let five = "5";
+let myLibrary = [];
 
+function book(name, author, pageCount, hasRead) {
+
+    this.name = name
+    this.author = author
+    this.pageCount = pageCount
+    this.hasRead = hasRead
+};
+
+book.prototype.addBook = function() {
+
+    
+    myLibrary.push(this)
+};
+
+book.prototype.removeBook = function() {
+
+    let index = myLibrary.findIndex(x => x.name === this.name)
+    myLibrary.splice(index, 1)
+};
